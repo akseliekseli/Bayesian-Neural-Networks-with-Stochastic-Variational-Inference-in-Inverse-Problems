654ab2f943c98b89e2dd3d9c58d057272b4b73cc
akseliekseli <akseli.suutari@hotmail.com>
2024-07-26 19:22:22 +0300

    .

--- codes/run_all_configs.py
+++ codes/run_all_configs.py
@@ -6,4 +6,4 @@ config = yaml.safe_load(open("codes/config/config.yml"))
 for type in config:
     for conf in config[type]:
         print(f'RUNNING CONFIG: {conf}\n')
-        os.system(f"python3 codes/main_bnn_prior.py --type type --config {conf}")
+        os.system(f"python3 codes/main_bnn_prior.py --type {type} --config {conf}")
