ce2130b9eec74b0de3bcc4011e7e839149e855dc
audioAXS <akseli.suutari@hotmail.com>
2024-08-11 22:35:26 +0300

    iterations tests

--- codes/run_all_configs.py
+++ codes/run_all_configs.py
@@ -6,18 +6,22 @@ config = yaml.safe_load(open("codes/config/config.yml"))
 
 # Parse the config argument
 parser = argparse.ArgumentParser(description="1D-deconvolution solving with BNN prior")
-parser.add_argument('--conf_family', type=str, required=True, help='Type of config, all to run all configs')
+parser.add_argument('--experiment', type=str, required=True, help='Type of config, all to run all configs')
 args = parser.parse_args()
 
-conf_family = args.conf_family
+experiment = args.experiment
 
-if conf_family == 'all':
+if experiment == 'all':
     for problem_type in config:
         for exp_type in config[problem_type]:
             for conf in config[problem_type][exp_type]:
                 print(f'RUNNING CONFIG: {conf}\n')
                 os.system(f"python3 codes/main_bnn_prior.py --problem_type {problem_type} --experiment_type {exp_type} --config {conf}")
 else:
-    for conf in config[conf_family]:
-        print(f'RUNNING CONFIG: {conf}\n')
-        os.system(f"python3 codes/main_bnn_prior.py --type {conf_family} --config {conf}")
+    for problem_type in config:
+        if problem_type == 'initial':
+            pass
+        else:
+            for conf in config[problem_type][experiment]:
+                print(f'RUNNING CONFIG: {conf}\n')
+                os.system(f"python3 codes/main_bnn_prior.py --problem_type {problem_type} --experiment_type {experiment} --config {conf}")
